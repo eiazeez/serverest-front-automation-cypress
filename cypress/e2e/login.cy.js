d911902b6fbcf5e303d46cb644878fe33764cb8b
azeez <isaac.douglas08@gmail.com>
2025-01-23 20:56:19 -0300

    :test_tube: test: Ajustando teste de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,22 @@
 
 
 it('Teste 01 - LOGIN', function(){
+
+    cy.api({
+        url: 'https://serverest.dev/usuarios',
+        method: 'POST',
+        failOnStatusCode: false,
+        body: {
+                "nome": "Isaac Douglas Aragao Eu sou o Douglas",
+                "email": "teste-isaac@qa.com.br",
+                "password": "teste",
+                "administrador": "false"
+            }
+    })
+
     cy.visit('https://front.serverest.dev/login')
-    cy.get('input[type=email]').type('email-de-verdade@gmail.com')
-    cy.get('input[type=password]').type('senhamuitoforte123456789hahaha')
+    cy.get('input[type=email]').type('teste-isaac@qa.com.br')
+    cy.get('input[type=password]').type('teste')
     cy.get('button[type="submit"]').click()
 
     cy.get('section[class="row espacamento"]').should('be.visible')
