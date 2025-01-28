c496de85755d771459b9f77b7f7b94ca4025937e
azeez <isaac.douglas08@gmail.com>
2025-01-28 20:35:06 -0300

    :wrench: Config: Ajustando url's base do Projeto

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://front.serverest.dev",
     viewportWidth:  1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
