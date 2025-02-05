609757a6bb6ff871a42b065ed7a3952d7ebe3239
azeez <isaac.douglas08@gmail.com>
2025-02-05 20:26:58 -0300

    :wrench: Config: Ajustando timeout

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     baseUrl: "https://front.serverest.dev",
     viewportWidth:  1920,
     viewportHeight: 1080,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
